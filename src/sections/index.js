9b7bba95e91cfa0d77ba575ed29a793ac0d14414
unknown <ajayvish936@gmail.com>
2024-02-14 18:10:59 +0530

    nike app

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Services from './Services';
+import Footer from './Footer';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import PopularProduct from './PopularProduct';
+import CustomerReviews from './CustomerReviews';
+
+export {
+    Hero,
+    Services,
+    Footer,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    PopularProduct,
+    CustomerReviews
+}
