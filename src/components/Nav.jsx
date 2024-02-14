61cd90f0887191e725ffd4fe1c89270b381f3624
unknown <ajayvish936@gmail.com>
2024-02-15 02:13:59 +0530

    nav Changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,30 +1,47 @@
 import {headerLogo} from '../assets/images';
 import {hamburger} from '../assets/icons';
 import {navLinks} from '../constants';
+import { useState } from 'react';
 const Nav = () => {
-    
-  return (
-    <header className='padding-x py-8 absolute z-10 w-full'>
-        <nav className='flex justify-between items-center max-container'> 
-            <a href="/" >
-                <img src={headerLogo} alt="logo" width={130} height={29} />
-            
-            </a>
-            <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-               {navLinks.map((item) => (
-                    <li key={item.label}> 
-                     <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">{item.label}</a>
-                    
-                    </li>
-               ))} 
+  // JSX
+const [showMenu, setShowMenu] = useState(false);
 
-            </ul>
-            <div className='hidden max-lg:block'>
-                <img src={hamburger} alt='hamburger' width={25} height={25}  />
-            </div>
-        </nav>
-    </header>
-  )
+const toggleMenu = () => {
+  setShowMenu(!showMenu);
+};
+
+return (
+  <header className='padding-x  py-8 absolute z-10 w-full'>
+    <nav className='flex justify-between items-center max-container'>
+      <a href="/">
+        <img src={headerLogo} alt="logo" width={130} height={29} />
+      </a>
+       
+        <ul className='flex flex-1 justify-center items-center gap-16 max-lg:hidden '>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">{item.label}</a>
+            </li>
+          ))}
+        </ul>
+      
+      <div className='lg:hidden'>
+        <img src={hamburger} alt='hamburger' width={25} height={25} onClick={toggleMenu} />
+      </div>
+      {showMenu && (
+        <div className=' absolute top-full right-0 bg-white mt-2 p-8 rounded-lg shadow-md lg:hidden  '>
+          <ul className='flex flex-col gap-4'>
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </nav>
+  </header>
+);
 }
 
 export default Nav
